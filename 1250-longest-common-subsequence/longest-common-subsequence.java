d978aad05402dd2626aae62a9b7ceaf85c2e1771
Naveen G G <naveengg743@gmail.com>
2025-09-26 12:32:08 +0530

    Time: 19 ms (87.80%) | Memory: 51.2 MB (21.23%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -13,8 +13,8 @@ class Solution {
             dp[0][j] = 0;
         }
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1;j<=m; j++) {
                 if (text1.charAt(i-1) == text2.charAt(j-1))
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
