daa90de321945c115bcad3a146db2b429d5f3754
Naveen G G <naveengg743@gmail.com>
2026-02-04 14:49:19 +0530

    Time: 2533 ms (13.00%) | Memory: 44.2 MB (72.65%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -2,9 +2,9 @@ class Solution {
     public int numTeams(int[] rating) {
         int n = rating.length;
         int count =0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i+1;j<n-1;j++)
             {
                 for(int k=j+1;k<n;k++)
                 {
