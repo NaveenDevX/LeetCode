e6e76bc1c41ef125fd3a6004677bb02deb4940a3
Naveen G G <naveengg743@gmail.com>
2026-02-17 20:59:15 +0530

    Time: 28 ms (26.70%) | Memory: 130 MB (14.87%) - LeetSync

---
+++ 4190-minimum-operations-to-reach-target-array/minimum-operations-to-reach-target-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums, int[] target) {
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=target[i])
+            {
+                if(!set.contains(nums[i]))
+                {
+                    set.add(nums[i]);
+                }
+            }
+        }
+        return set.size();
+    }
+}
