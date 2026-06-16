9f320dcd11cf58c4c98cacabd6f71b4ea1f6f315
Naveen G G <naveengg743@gmail.com>
2026-06-16 14:14:03 +0530

    Time: 17 ms (16.06%) | Memory: 48.3 MB (8.62%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }    
+        int duplicate = -1, missing = -1;
+        
+        for (int i = 1; i <= n; i++) {
+            int count = map.getOrDefault(i, 0);
+            if (count == 2) {
+                duplicate = i;
+            } else if (count == 0) {
+                missing = i;
+            }
+        }
+        
+        return new int[]{duplicate, missing};
+    }
+}
