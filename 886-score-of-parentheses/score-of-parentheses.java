2f9e6a0d60fa9f326582cb363c005b3edd655cf7
Naveen G G <naveengg743@gmail.com>
2026-06-21 16:50:55 +0530

    Time: 1 ms (61.24%) | Memory: 42.8 MB (28.40%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        int count =0;
+        Stack<Integer> st = new Stack<>();
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '(')
+            {
+                st.push(count);
+                count =0;
+            }
+            else{
+                count = st.pop() + Math.max(2*count,1);
+            }
+        }
+        return count;
+    }
+}
