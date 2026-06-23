6b8ffb788b17c87368e120c30383a771a3943ccc
Naveen G G <naveengg743@gmail.com>
2026-06-23 10:34:15 +0530

    Time: 5 ms (31.46%) | Memory: 47 MB (48.30%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]%2==0)
+            {
+                ans[i] = 0;
+            }
+            else{
+                ans[i] = 1;
+            }
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}
