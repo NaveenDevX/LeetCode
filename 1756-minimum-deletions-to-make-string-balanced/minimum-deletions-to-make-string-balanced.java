87fc0574a564872badcb64cda7bbe28de36b18f2
Naveen G G <naveengg743@gmail.com>
2026-06-29 10:09:49 +0530

    Time: 128 ms (6.23%) | Memory: 47.8 MB (47.71%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        Stack<Character>st = new Stack<>();
+        int count = 0;
+        for(char ch : s.toCharArray())
+        {
+           if(!st.isEmpty() && (st.peek() == 'b' && ch == 'a'))
+           {
+               st.pop();
+               count++;
+           }
+           else{
+               st.push(ch);
+           }
+        }
+        return count;
+    }
+}
