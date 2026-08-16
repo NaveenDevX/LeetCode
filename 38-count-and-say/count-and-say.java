436ac08c629dbe2c8c659d34c294a65911c53ed7
Naveen G G <naveengg743@gmail.com>
2026-08-17 01:33:42 +0530

    Time: 8 ms (61.56%) | Memory: 43.1 MB (55.37%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        String ans = "1";
+        for(int i=1;i<n;i++)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for(int j=1;j<ans.length();j++)
+            {
+                if(ans.charAt(j) == ans.charAt(j-1))
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.append(count).append(ans.charAt(j-1));
+                    count = 1;
+                }
+            }
+            sb.append(count).append(ans.charAt(ans.length()-1));
+
+            ans = sb.toString();
+        }
+
+        return ans;
+    }
+}
